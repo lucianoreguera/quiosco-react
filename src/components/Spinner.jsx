a0755b6a88812d32e9585eaa2081650606561c2b
lucianor <regueraluciano@gmail.com>
2023-03-30 17:47:19 -0300

    Se centra el Spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,13 +2,15 @@ import '../styles/Spinner.css'
 
 const Spinner = () => {
   return (
-    <div className="sk-chase">
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
-      <div className="sk-chase-dot"></div>
+    <div className="flex justify-center items-center mt-96">
+      <div className="sk-chase">
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+      </div>
     </div>
   )
 }
